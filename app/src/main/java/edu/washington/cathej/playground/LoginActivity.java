320241804d245b44e4e464a28b5dcb8eddaeed89
Cat <cathej6@gmail.com>
2017-03-31 10:34:42 -0700

    Added two more activites for Designers to play with. Only thing on these pages are one button, next step will be fleshing out these pages.

--- app/src/main/java/edu/washington/cathej/playground/LoginActivity.java
+++ app/src/main/java/edu/washington/cathej/playground/LoginActivity.java
@@ -1,18 +1,18 @@
 package edu.washington.cathej.playground;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.app.Activity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         Button button = (Button) findViewById(R.id.button);
 
@@ -20,9 +20,10 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Log.i("button", "Text updated.");
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                MainActivity.this.startActivity(intent);
+                Intent intent = new Intent(LoginActivity.this, ParentActivity.class);
+                LoginActivity.this.startActivity(intent);
             }
         });
     }
+
 }
