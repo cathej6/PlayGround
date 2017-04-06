5372ac3c18d75e0f00cc9a5398e88346c50abb4a
Cat <cathej6@gmail.com>
2017-04-06 11:28:29 -0700

    Adding addtional activities and the prototype works on phones now.

---
+++ app/src/main/java/edu/washington/cathej/playground/PreferencesActivity.java
@@ -0,0 +1,17 @@
+package edu.washington.cathej.playground;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class PreferencesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+
+
+        
+    }
+
+}
