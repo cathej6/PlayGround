c8b68248987867b67bbde0201090afb57bf6f6e5
Cat <cathej6@gmail.com>
2017-04-21 10:44:21 -0700

    Got the background image to work, just need to always have a lower resilution.

--- app/src/main/java/edu/washington/cathej/playground/MapActivity.java
+++ app/src/main/java/edu/washington/cathej/playground/MapActivity.java
@@ -14,8 +14,6 @@ public class MapActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
-        ImageButton one = (ImageButton) findViewById(R.id.imageButton);
-        one.setOnClickListener(this);
         ImageButton two = (ImageButton) findViewById(R.id.imageButton2);
         two.setOnClickListener(this);
         ImageButton start = (ImageButton) findViewById(R.id.imageButton3);
@@ -24,10 +22,6 @@ public class MapActivity extends Activity implements View.OnClickListener {
         four.setOnClickListener(this);
         ImageButton five = (ImageButton) findViewById(R.id.imageButton5);
         five.setOnClickListener(this);
-        ImageButton six = (ImageButton) findViewById(R.id.imageButton6);
-        six.setOnClickListener(this);
-        ImageButton seven = (ImageButton) findViewById(R.id.imageButton7);
-        seven.setOnClickListener(this);
     }
 
     @Override
