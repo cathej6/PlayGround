6d2f260a844f0e2d6c83acf648ffbd4f531a0996
Cat <cathej6@gmail.com>
2017-04-21 09:08:31 -0700

    Just updated some stuff, and getting ready to merge

--- app/src/main/java/edu/washington/cathej/playground/MapActivity.java
+++ app/src/main/java/edu/washington/cathej/playground/MapActivity.java
@@ -9,6 +9,9 @@ public class MapActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+
+
     }
 
 }
