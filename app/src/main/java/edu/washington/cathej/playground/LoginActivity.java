c2f73f89ef31152e5ffb4a3384f207744303b4d4
Cat <cathej6@gmail.com>
2017-05-11 10:33:58 -0700

    Got the main page with the characters crowded around a book clickable

--- app/src/main/java/edu/washington/cathej/playground/LoginActivity.java
+++ app/src/main/java/edu/washington/cathej/playground/LoginActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class LoginActivity extends Activity {
@@ -16,6 +17,7 @@ public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
 
         sharedPreferences = getSharedPreferences("playGroundPref", Context.MODE_PRIVATE);
--- app/src/main/java/edu/washington/cathej/playground/MainActivity.java
+++ app/src/main/java/edu/washington/cathej/playground/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
@@ -20,17 +21,19 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
 
         sharedPreferences = getSharedPreferences("playGroundPref", Context.MODE_PRIVATE);
         loggedIn = sharedPreferences.getBoolean("loggedIn", false);
 
-        Log.i("loggin", "loggedIn bool: " + loggedIn);
+        //Log.i("loggin", "loggedIn bool: " + loggedIn);
 
-        if (!loggedIn) {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-        }
+        //if (!loggedIn) {
+            //Intent intent = new Intent(this, LoginActivity.class);
+            //startActivity(intent);
+        //}
 
         // Goes to Parent Mode
         Button pModeButton = (Button) findViewById(R.id.pMode);
@@ -44,8 +47,19 @@ public class MainActivity extends Activity {
         });
 
         // Goes to Child Mode
-        Button cModeButton = (Button) findViewById(R.id.cMode);
-        cModeButton.setOnClickListener(new View.OnClickListener() {
+        Button cModeButton1 = (Button) findViewById(R.id.cMode1);
+        cModeButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("button", "Text updated.");
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                MainActivity.this.startActivity(intent);
+            }
+        });
+
+        // Goes to Child Mode
+        Button cModeButton2 = (Button) findViewById(R.id.cMode2);
+        cModeButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i("button", "Text updated.");
