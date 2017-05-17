f84e4a240edbf9e8d681b4b2ea7b20c03a7c5f52
Cat <cathej6@gmail.com>
2017-05-17 09:24:12 -0700

    Added ability to click on the branches and highlight them accordingly.

--- app/src/main/java/edu/washington/cathej/playground/ParentActivity.java
+++ app/src/main/java/edu/washington/cathej/playground/ParentActivity.java
@@ -9,9 +9,11 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TabHost;
 
 import static android.os.Build.VERSION_CODES.M;
+import static edu.washington.cathej.playground.R.drawable.lbg;
 
 public class ParentActivity extends Activity {
 
@@ -39,6 +41,28 @@ public class ParentActivity extends Activity {
         spec.setContent(R.id.tab3);
         spec.setIndicator("Fred");
         host.addTab(spec);
+
+
+        // Branch buttons.
+        Button lb = (Button) findViewById(R.id.lbbutton);
+        Button rt = (Button) findViewById(R.id.rtbutton);
+        Button lt = (Button) findViewById(R.id.ltbutton);
+        Button lm = (Button) findViewById(R.id.lmbutton);
+
+        Button lb2 = (Button) findViewById(R.id.lbbutton2);
+        Button rt2 = (Button) findViewById(R.id.rtbutton2);
+        Button lt2 = (Button) findViewById(R.id.ltbutton2);
+        Button lm2 = (Button) findViewById(R.id.lmbutton2);
+
+        Button lb3 = (Button) findViewById(R.id.lbbutton3);
+        Button rt3 = (Button) findViewById(R.id.rtbutton3);
+        Button lt3 = (Button) findViewById(R.id.ltbutton3);
+        Button lm3 = (Button) findViewById(R.id.lmbutton3);
+
+        onClick1(lb, rt, lt, lm);
+        onClick2(lb2, rt2, lt2, lm2);
+        onClick3(lb3, rt3, lt3, lm3);
+
     }
 
     @Override
@@ -76,4 +100,158 @@ public class ParentActivity extends Activity {
         intent.putExtra("mode", mode);
         ParentActivity.this.startActivity(intent);
     }
+
+
+    public void onClick1(Button lb, Button rt, Button lt, Button lm) {
+
+        lb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView gImage = (ImageView) findViewById(R.id.lbg);
+                if (gImage.getVisibility() == View.GONE) {
+                    gImage.setVisibility(View.VISIBLE);
+                } else {
+                    gImage.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        rt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView gImage = (ImageView) findViewById(R.id.rtg);
+                if (gImage.getVisibility() == View.GONE) {
+                    gImage.setVisibility(View.VISIBLE);
+                } else {
+                    gImage.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        lt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView gImage = (ImageView) findViewById(R.id.ltg);
+                if (gImage.getVisibility() == View.GONE) {
+                    gImage.setVisibility(View.VISIBLE);
+                } else {
+                    gImage.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        lm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView gImage = (ImageView) findViewById(R.id.lmg);
+                if (gImage.getVisibility() == View.GONE) {
+                    gImage.setVisibility(View.VISIBLE);
+                } else {
+                    gImage.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    public void onClick2(Button lb2, Button rt2, Button lt2, Button lm2) {
+
+        lb2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView gImage = (ImageView) findViewById(R.id.lbg2);
+                if (gImage.getVisibility() == View.GONE) {
+                    gImage.setVisibility(View.VISIBLE);
+                } else {
+                    gImage.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        rt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView gImage = (ImageView) findViewById(R.id.rtg2);
+                if (gImage.getVisibility() == View.GONE) {
+                    gImage.setVisibility(View.VISIBLE);
+                } else {
+                    gImage.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        lt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView gImage = (ImageView) findViewById(R.id.ltg2);
+                if (gImage.getVisibility() == View.GONE) {
+                    gImage.setVisibility(View.VISIBLE);
+                } else {
+                    gImage.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        lm2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView gImage = (ImageView) findViewById(R.id.lmg2);
+                if (gImage.getVisibility() == View.GONE) {
+                    gImage.setVisibility(View.VISIBLE);
+                } else {
+                    gImage.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    public void onClick3(Button lb3, Button rt3, Button lt3, Button lm3) {
+        lb3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView gImage = (ImageView) findViewById(R.id.lbg3);
+                if (gImage.getVisibility() == View.GONE) {
+                    gImage.setVisibility(View.VISIBLE);
+                } else {
+                    gImage.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        rt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView gImage = (ImageView) findViewById(R.id.rtg3);
+                if (gImage.getVisibility() == View.GONE) {
+                    gImage.setVisibility(View.VISIBLE);
+                } else {
+                    gImage.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        lt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView gImage = (ImageView) findViewById(R.id.ltg3);
+                if (gImage.getVisibility() == View.GONE) {
+                    gImage.setVisibility(View.VISIBLE);
+                } else {
+                    gImage.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        lm3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageView gImage = (ImageView) findViewById(R.id.lmg3);
+                if (gImage.getVisibility() == View.GONE) {
+                    gImage.setVisibility(View.VISIBLE);
+                } else {
+                    gImage.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
 }
